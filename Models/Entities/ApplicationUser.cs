a6ce889bf2c9d88af01a6e10bb45ff3a2b748ffc
MalikBogos <126606030+MalikBogos@users.noreply.github.com>
2026-04-01 19:31:10 +0200

    - Added ApplicationUser Model - Added Customer Model

---
+++ Models/Entities/ApplicationUser.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+
+namespace Models.Entities
+{
+	public class ApplicationUser : IdentityUser
+	{
+		// Necessary
+		//[ProtectedPersonalData]
+		[Required(ErrorMessage = "FirstName is required")]
+		[StringLength(50)] //Programming standard
+		public required string FirstName { get; set; }
+
+		// Necessary
+		//[ProtectedPersonalData]
+		[Required(ErrorMessage = "LastName is required")]
+		[StringLength(100)] //Programming standard
+		public required string LastName { get; set; }
+
+		// In case a user gets fired, for example (set instead of init in case user needs to be unblocked)
+		public bool IsBlocked { get; set; } = false;
+
+		[StringLength(250, ErrorMessage = "Block reason should be 250 characters at most")]
+		public string? BlockReason { get; set; }
+
+		public DateTime? BlockedAt { get; set; }
+
+		// Necessary information for account info
+		[Required(ErrorMessage = "Application User creation date is required")]
+		public required DateTime CreatedAt { get; init; } = DateTime.UtcNow;
+
+		// Soft-delete
+		//[Required(ErrorMessage = "IsDeleted of Application User is required")]
+		public bool IsDeleted { get; set; } = false;
+
+		// Necessary information for account info
+		public DateTime? DeletedAt { get; set; }
+	}
+}
---
+++ Models/Entities/Customer.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Models.Entities
+{
+	public class Customer
+	{
+		[Key]
+		public required int Id { get; init; }
+
+		[Required(ErrorMessage = "Customer's FirstName is required")]
+		[StringLength(100)]
+		public required string FirstName { get; set; }
+
+		
+		[Required(ErrorMessage = "Customer's LastName is required")]
+		[StringLength(100)]
+		public required string LastName { get; set; }
+
+		[Required(ErrorMessage = "Customer's Email is required")]
+		[StringLength(200)]
+		[EmailAddress]
+		public required string Email { get; set; }
+
+		[ProtectedPersonalData]
+		[Phone]
+		[Required(ErrorMessage = "Cannot contact customer without Customer's PhoneNumber")]
+		public required string PhoneNumber { get; set; }
+
+		[Required(ErrorMessage = "Customer's Address is required")] // Cannot work without knowing the address
+		public required string Address { get; set; }
+		
+		public DateTime CreatedAt { get; init; } = DateTime.UtcNow;
+
+		public bool IsDeleted { get; set; } = false;
+
+		public DateTime? DeletedAt { get; set; }
+	}
+}

78c53be705e0b30f89137b3ed7dd7138af2b7ebd
MalikBogos <126606030+MalikBogos@users.noreply.github.com>
2026-04-11 22:52:38 +0200

    - Added Enums folder to add different Status values

---
+++ Models/Enums/ProjectStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models.Enums
+{
+	public enum ProjectStatus
+	{
+		Pending,
+		Active,
+		Paused,
+		Completed,
+		Cancelled,
+	}
+}
---
+++ Models/Enums/QuoteStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models.Enums
+{
+	public enum QuoteStatus
+	{
+		Pending,
+		Active,
+		Paused,
+		Completed,
+		Cancelled
+	}
+}

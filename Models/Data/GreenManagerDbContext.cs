81efcd9aedb8dcd709ad686379fc117c5faa77b6
MalikBogos <126606030+MalikBogos@users.noreply.github.com>
2026-04-21 16:22:13 +0200

    - Installed Microsoft.EntityFrameworksCore.Tools in Models (Models.Entities) - Created GreenManagerDbContext (Models.Data)

---
+++ Models/Data/GreenManagerDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models.Data
+{
+	public class GreenManagerDbContext : IdentityDbContext<ApplicationUser>
+	{
+	}
+}

4a5fd55b88a9dad1814e7b294919ccaad1363834
MalikBogos <126606030+MalikBogos@users.noreply.github.com>
2026-04-21 18:31:56 +0200

    - Added constructor and DbSet configuration to the DbContext

--- Models/Data/GreenManagerDbContext.cs
+++ Models/Data/GreenManagerDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Models.Entities;
 using System;
 using System.Collections.Generic;
@@ -8,5 +9,25 @@ namespace Models.Data
 {
 	public class GreenManagerDbContext : IdentityDbContext<ApplicationUser>
 	{
+		public GreenManagerDbContext(DbContextOptions<GreenManagerDbContext> options) : base(options) 
+		{
+
+		}
+
+		public DbSet<Address> Addresses { get; set; }
+		public DbSet<Customer> Customers { get; set; }
+		public DbSet<Employee> Employees { get; set; }
+		public DbSet<EmployeeWageHistory> EmployeeWageHistories { get; set; }
+		public DbSet<Material> Materials { get; set; }
+		public DbSet<MaterialCategory> MaterialCategories { get; set; }
+		public DbSet<Project> Projects { get; set; }
+		public DbSet<ProjectEmployee> ProjectEmployees { get; set; }
+		public DbSet<ProjectMaterial> ProjectMaterials { get; set; }
+		public DbSet<ProjectTask> ProjectTasks { get; set; }
+		public DbSet<Quote> Quotes { get; set; }
+		public DbSet<QuoteItem> QuoteItems { get; set; }
+		public DbSet<WorkLog> WorkLogs { get; set; }
+
+
 	}
 }

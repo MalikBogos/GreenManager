44a7b4194c43c2362962ff36553009ecf59c768d
MalikBogos <126606030+MalikBogos@users.noreply.github.com>
2026-04-27 23:49:57 +0200

    - Moved MainWindow to Views (Wpf.Views) - Created MainViewModel (Wpf.MainViewModels) - Deleted redundant file - Updated references

---
+++ GreenManager - WPF/ViewModels/MainViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Models;
+using Models.Data;
+
+namespace GreenManager___WPF.ViewModels
+{
+
+	public class MainViewModel
+	{
+		public MainViewModel()
+		{
+			Customer malik = new Customer() { FirstName = "Malik", LastName = "Bogos", Email = "malikbogos@gmail.com", PhoneNumber = "23844848023" };
+
+			using (var context = new GreenManagerDbContext())
+			{
+				context.Add(malik);
+				context.SaveChanges();
+			}
+
+		}
+	}
+
+}
--- GreenManager - WPF/Views/MainWindow.xaml.cs
+++ GreenManager - WPF/Views/MainWindow.xaml.cs
@@ -9,12 +9,12 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace GreenManager___WPF
+namespace GreenManager___WPF.Views
 {
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
-	public partial class MainWindow : NavigationWindow
+	public partial class MainWindow : Window
 	{
 		public MainWindow()
 		{

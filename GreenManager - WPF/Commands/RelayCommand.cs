eae22ac0a2f638502bd939d4f5a60542f10b99a3
MalikBogos <126606030+MalikBogos@users.noreply.github.com>
2026-05-14 23:26:34 +0200

    - Created RelayCommands (Wpf.Commands)

---
+++ GreenManager - WPF/Commands/RelayCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace GreenManager___WPF.Commands
+{
+	public class RelayCommand : ICommand
+	{
+		private readonly Action _execute;
+		public RelayCommand(Action execute) => _execute = execute;
+		public bool CanExecute(object? parameter) => true;
+		public void Execute(object? parameter) => _execute();
+		public event EventHandler? CanExecuteChanged;
+	}
+}

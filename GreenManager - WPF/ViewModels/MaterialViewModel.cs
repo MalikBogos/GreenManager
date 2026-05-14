d4386eb3bc44ca6ee50fa345634d71354b4027e4
MalikBogos <126606030+MalikBogos@users.noreply.github.com>
2026-05-14 23:56:55 +0200

    - Added logic to create new Material row (Wpf.ViewModels) - Updated MaterialView to include textboxes for materials creation (Wpf.Views)

--- GreenManager - WPF/ViewModels/MaterialViewModel.cs
+++ GreenManager - WPF/ViewModels/MaterialViewModel.cs
@@ -1,9 +1,11 @@
-﻿using Models.Data;
+﻿using GreenManager___WPF.Commands;
+using Models.Data;
 using Models.Entities;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Text;
+using System.Windows.Input;
 
 namespace GreenManager___WPF.ViewModels
 {
@@ -11,10 +13,17 @@ namespace GreenManager___WPF.ViewModels
 	{
 		public ObservableCollection<Material> Materials { get; set; }
 
+		public string NewName { get; set; } = "";
+		public string NewUnit { get; set; } = "";
+		public decimal NewPrice { get; set; }
+		public decimal NewStock { get; set; }
+
+		public ICommand SaveCommand { get; }
+
 		public MaterialViewModel()
 		{
 			Materials = new ObservableCollection<Material>();
-
+			SaveCommand = new RelayCommand(SaveMaterial);
 			LoadMaterials();
 		}
 
@@ -29,6 +38,28 @@ namespace GreenManager___WPF.ViewModels
 					Materials.Add(materials);
 				}
 			}
+		}
+
+		private void SaveMaterial()
+		{
+			if (string.IsNullOrWhiteSpace(NewName)) return;
+
+			using (var context = new GreenManagerDbContext())
+			{
+				var m = new Material
+				{
+					Name = NewName,
+					Unit = NewUnit,
+					PurchasePrice = NewPrice,
+					StockQuantity = NewPrice,
+					CreatedAt = DateTime.UtcNow
+				};
+
+				context.Materials.Add(m);
+				context.SaveChanges();
+			}
+
+			LoadMaterials();
 		}
 	}
 }

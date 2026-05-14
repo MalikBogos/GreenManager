dcda278e420382a453fbe243af4c865de76861cb
MalikBogos <126606030+MalikBogos@users.noreply.github.com>
2026-05-14 21:20:03 +0200

    - Deleted redundant application resources (Wpf.App) - Updated CultureInfo of the project (Wpf.App)

--- GreenManager - WPF/App.xaml.cs
+++ GreenManager - WPF/App.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System.Configuration;
 using System.Data;
+using System.Globalization;
 using System.Windows;
+using System.Windows.Markup;
 
 namespace GreenManager___WPF
 {
@@ -9,6 +11,19 @@ namespace GreenManager___WPF
 	/// </summary>
 	public partial class App : Application
 	{
+		protected override void OnStartup(StartupEventArgs e)
+		{
+			base.OnStartup(e);
+
+			var culture = new CultureInfo("nl-BE");
+
+			Thread.CurrentThread.CurrentCulture = culture;
+			Thread.CurrentThread.CurrentUICulture = culture;
+
+			FrameworkElement.LanguageProperty.OverrideMetadata(
+				typeof(FrameworkElement),
+				new FrameworkPropertyMetadata(XmlLanguage.GetLanguage(culture.IetfLanguageTag)));
+		}
 	}
 
 }

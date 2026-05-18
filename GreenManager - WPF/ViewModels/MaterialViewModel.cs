6b2348a60479a4b1eee472c63f2fb3f1b42ddf0e
MalikBogos <126606030+MalikBogos@users.noreply.github.com>
2026-05-18 17:29:14 +0200

    - Created EditMaterialWindow which appears when editing materials (Wpf.Views) - Updated layout to accommodate new buttons for editing & deleting (Wpf.Views) - Added logic for buttons to edit & delete materials from the list  (Wpf.ViewModels)

--- GreenManager - WPF/ViewModels/MaterialViewModel.cs
+++ GreenManager - WPF/ViewModels/MaterialViewModel.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Text;
+using System.Windows;
 using System.Windows.Input;
 
 namespace GreenManager___WPF.ViewModels
@@ -13,14 +14,24 @@ namespace GreenManager___WPF.ViewModels
 	public class MaterialViewModel
 	{
 		public ObservableCollection<Material> Materials { get; set; }
+
+		public Material SelectedMaterial { get; set; }
+
 		public ICommand OpenAddWindowCommand { get; }
+
+		public ICommand EditCommand { get; }
+
+		public ICommand DeleteCommand { get; }
 
 		public MaterialViewModel()
 		{
 			Materials = new ObservableCollection<Material>();
 			OpenAddWindowCommand = new RelayCommand(OpenAddWindow);
+			EditCommand = new RelayCommand(EditMaterial);
+			DeleteCommand = new RelayCommand(DeleteMaterial);
 			LoadMaterials();
 		}
+
 
 		private void LoadMaterials()
 		{
@@ -50,6 +61,52 @@ namespace GreenManager___WPF.ViewModels
 					context.Materials.Add(materialToSave);
 					context.SaveChanges();
 				}
+				LoadMaterials();
+			}
+		}
+
+		private void EditMaterial()
+		{
+			if (SelectedMaterial == null)
+			{
+				MessageBox.Show("Selecteer eerst een materiaal om te bewerken.", "Geen selectie", MessageBoxButton.OK, MessageBoxImage.Information);
+				return;
+			}
+
+			var editWindow = new EditMaterialWindow(SelectedMaterial);
+
+			if(editWindow.ShowDialog() == true)
+			{
+				using (var context = new GreenManagerDbContext())
+				{
+					context.Materials.Update(editWindow.EditedMaterial);
+					context.SaveChanges();
+				}
+				LoadMaterials();
+			}
+		}
+
+		private void DeleteMaterial()
+		{
+			// 1. Controleer of er wel iets is geselecteerd
+			if (SelectedMaterial == null)
+			{
+				MessageBox.Show("Selecteer eerst een materiaal uit de lijst.", "Geen selectie", MessageBoxButton.OK, MessageBoxImage.Information);
+				return;
+			}
+
+			// 2. Vraag om bevestiging (Best practice!)
+			var result = MessageBox.Show($"Weet je zeker dat je '{SelectedMaterial.Name}' wilt verwijderen?", "Verwijderen bevestigen", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+
+			if (result == MessageBoxResult.Yes)
+			{
+				using (var context = new GreenManagerDbContext())
+				{
+					context.Materials.Remove(SelectedMaterial);
+					context.SaveChanges();
+				}
+
+				// 3. Ververs de lijst zodat hij van het scherm verdwijnt
 				LoadMaterials();
 			}
 		}
---
+++ GreenManager - WPF/Views/EditMaterialWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GreenManager___WPF.Views
+{
+    /// <summary>
+    /// Interaction logic for EditMaterialWindow.xaml
+    /// </summary>
+    public partial class EditMaterialWindow : Window
+    {
+		public Material EditedMaterial { get; set; }
+
+		public EditMaterialWindow(Material selectedMaterial)
+		{
+			InitializeComponent();
+
+			// We maken een KOPIE van de data. Zo verpesten we de tabel niet als iemand op annuleren drukt.
+			EditedMaterial = new Material
+			{
+				Id = selectedMaterial.Id, // ID moet behouden blijven voor de Update!
+				Name = selectedMaterial.Name,
+				Description = selectedMaterial.Description,
+				PurchasePrice = selectedMaterial.PurchasePrice,
+				StockQuantity = selectedMaterial.StockQuantity,
+				Unit = selectedMaterial.Unit,
+				Notes = selectedMaterial.Notes,
+				CreatedAt = selectedMaterial.CreatedAt // Behoud de originele datum
+			};
+
+			this.DataContext = this;
+		}
+
+		private void BtnSave_Click(object sender, RoutedEventArgs e)
+		{
+			this.DialogResult = true;
+		}
+
+		private void BtnCancel_Click(object sender, RoutedEventArgs e)
+		{
+			this.DialogResult = false;
+		}
+	}
+}

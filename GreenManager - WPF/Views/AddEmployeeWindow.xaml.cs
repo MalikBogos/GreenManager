c9501bc25c4e5ee72917e9808c5025a2866f56c2
MalikBogos <126606030+MalikBogos@users.noreply.github.com>
2026-08-01 00:01:53 +0200

    - Created AddEmployeeWindow with incomplete code-behind (Wpf.Views)

---
+++ GreenManager - WPF/Views/AddEmployeeWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GreenManager___WPF.Views
+{
+	/// <summary>
+	/// Interaction logic for AddEmployeeWindow.xaml
+	/// </summary>
+	public partial class AddEmployeeWindow : Window
+	{
+		public Employee NewEmployee { get; set; }
+
+		public AddEmployeeWindow()
+		{
+			InitializeComponent();
+
+			// Bereid een leeg werknemer-object voor.
+			//NewEmployee = new Employee
+			//{
+			//	EmployeeNumber = "",
+			//	JobTitle = "",
+			//	HireDate = DateTime.Today,
+			//	CreatedAt = DateTime.UtcNow
+			//};
+
+			this.DataContext = this;
+		}
+
+		private void BtnSave_Click(object sender, RoutedEventArgs e)
+		{
+			if (string.IsNullOrWhiteSpace(NewEmployee.EmployeeNumber) || string.IsNullOrWhiteSpace(NewEmployee.JobTitle))
+			{
+				MessageBox.Show("Vul de verplichte velden (Personeelsnummer en Functie) in.", "Validatie", MessageBoxButton.OK, MessageBoxImage.Warning);
+				return;
+			}
+
+			this.DialogResult = true;
+		}
+
+		private void BtnCancel_Click(object sender, RoutedEventArgs e)
+		{
+			this.DialogResult = false;
+		}
+	}
+}
